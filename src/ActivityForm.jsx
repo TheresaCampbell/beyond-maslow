bd6a47b776fa6484f190649f3ff2bbd0ff146628
Theresa Campbell <t.campbell@sasktel.net>
2018-10-12 20:26:12 +0000

    In Activity Form component, each activity's state is inherited from List's state. This state is updated through the handleInput() function. The changes aren't saving yet.

--- src/ActivityForm.jsx
+++ src/ActivityForm.jsx
@@ -4,22 +4,33 @@ import axios from 'axios';
 class ActivityForm extends Component {
   constructor(props) {
     super(props)
-    this.state = {}
+    this.state = {
+      title: this.props.activity.title,
+      body: this.props.activity.body,
+      cost: this.props.activity.cost,
+      category: this.props.activity.category,
+      completed: this.props.activity.completed
+    }
   }
 
+  //When an activity is updated through the form, its state value changes accordingly.
+  handleInput = (e) => {
+    this.setState({[e.target.name]: e.target.value})
+  }
+
+  //The activity form, used to edit individual activities.
   render() {
     return(
       <div className="activity">
         <form>
-          <input className="title"></input>
-          <textarea className="body"></textarea>
-          <div className="cost">
-            <button>Free</button>
-            <button>$</button>
-            <button>$$</button>
-            <button>$$$</button>
-          </div>
-          <input className="categories"></input>
+          <input name="title" type="text" placeholder='Title'
+          value={this.state.title} onChange={this.handleInput}/>
+          <textarea name="body" placeholder='Description'
+          value={this.state.body} onChange={this.handleInput}></textarea>
+          <input name="cost" type="text" placeholder='Cost'
+          value={this.state.cost} onChange={this.handleInput}/>
+          <input name="category" type="text" placeholder='Category'
+          value={this.state.category} onChange={this.handleInput}/>
         </form>
       </div>
     )
